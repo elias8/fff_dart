bb097da2a588cc59f3e2bdb411d3933bbb6e07d5
Elias Andualem <eliasandualem8@gmail.com>
2026-09-23 21:35:22 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# fff_dart
+
+A pure Dart package for bindings to the [FFF](https://github.com/dmtrKovalenko/fff) C ABI. The binding is under development.
---
+++ example/fff_dart_example.dart
@@ -0,0 +1,6 @@
+import 'package:fff_dart/fff_dart.dart';
+
+void main() {
+  final awesome = Awesome();
+  assert(awesome.isAwesome, 'The example should be awesome.');
+}
